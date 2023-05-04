d8652491a360fd652ac51771af97258c74daaf11
qu4ks <egorlapatskij@gmail.com>
2023-05-04 23:22:08 +0300

    Basic processing logic processing initial commands

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import os
 import configparser
-def createConfig():
+import telebot
+
+from moviepy.editor import *
+
+
+def create_config():
     config_file = 'config.ini'
     if not os.path.isfile(config_file):
         config = configparser.ConfigParser()
@@ -11,6 +15,17 @@ def createConfig():
         with open(config_file, 'w') as configfile:
             config.write(configfile)
 def main():
-    createConfig()
+    create_config()
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    bot = telebot.TeleBot(config.get('Settings', 'bot_token'))
+
+    @bot.message_handler(commands=['help', 'start'])
+    def get_text_messages(message):
+        bot.send_message(message.from_user.id, "Пришли мне изображение которое ты хотел бы обработать")
+
+    bot.infinity_polling()
+
+
 if __name__ == '__main__':
     main()
