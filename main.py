4e5836c368a1be0df8853dd9eee8fc5e794d6007
qu4ks <egorlapatskij@gmail.com>
2023-05-06 22:49:56 +0300

    Черновая гиф анимация с нужными размерами.

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 import configparser
 import telebot
+from PIL import Image, ImageSequence
 
 from moviepy.editor import *
+
+
+def create_frame_array(gif_path):
+    frames = []
+
+    with Image.open(gif_path) as im:
+        # Итерируемся по всем кадрам GIF-анимации
+        for frame in range(0, im.n_frames):
+            im.seek(frame)  # Переходим к текущему кадру
+            frame_data = im.copy()  # Создаем копию кадра
+            frames.append(frame_data)  # Добавляем кадр в массив
+
+    return frames
 
 
 def create_config():
@@ -15,6 +29,30 @@ def create_config():
         with open(config_file, 'w') as configfile:
             config.write(configfile)
         sys.exit()
+
+
+def resize():
+    with Image.open('temp.gif') as im:
+        # Изменяем размер исходной гиф-анимации, чтобы ширина была равна 1856
+        wpercent = (1856 / float(im.size[0]))
+        hsize = int((float(im.size[1]) * float(wpercent)))
+        source_frames = create_frame_array('temp.gif')
+        frames_resized = []
+        for frame in source_frames:
+            frame1 = frame.resize((1856, hsize))
+            frames_resized.append(frame1)
+
+        # Создаем новую гиф-анимацию с заданными параметрами
+        frames = []
+        duration = []
+        for frame in frames_resized:
+            new_frame = Image.new('RGBA', (1920, hsize + 396), (0, 0, 0, 0))
+            new_frame.paste(frame, ((1920 - frame.width) // 2, 354))
+            frames.append(new_frame)
+            duration.append(frame.info['duration'])
+
+        # Сохраняем новую гиф-анимацию
+        frames[0].save('new.gif', save_all=True, append_images=frames[1:], loop=0, duration=duration)
 
 
 def main():
@@ -36,6 +74,7 @@ def main():
             with open("temp.gif", 'wb') as f:
                 f.write(file)
             bot.reply_to(message, "Гиф успешно сохранена! Обрабатываю")
+            resize()
 
     @bot.message_handler(content_types=['animation'])
     def handle_animation(message):
@@ -47,6 +86,7 @@ def main():
         clip = VideoFileClip('temp.mp4')
         clip.write_gif('temp.gif')
         bot.reply_to(message, "Гиф успешно сохранена! Обрабатываю")
+        resize()
 
     bot.infinity_polling()
 
